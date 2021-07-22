0a8c903fcfce59793b465fe4548744d3f627ed77
rzvr <work.folder.d@protonmail.com>
2021-07-22 23:01:07 +0200

    Add keyboard functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
-const SLIDER = document.querySelector('.slider');
 const ROOT_ELEMENT = document.documentElement;
-const CALC_DISPLAY = document.querySelector('.calc__display');
-const CALC_BODY = document.querySelector('.calc__body');
+const SLIDER = document.querySelector('.slider');
+const DISPLAY = document.querySelector('.calc__display');
+const KEYPAD = document.querySelector('.calc__keypad');
 
 let themes = {
     1: 'neutral',
@@ -9,37 +9,7 @@ let themes = {
     3: 'dark'
 }
 
-function switchTheme() {
-    ROOT_ELEMENT.setAttribute('data-theme', themes[sessionStorage.getItem('theme')]);
-    SLIDER.value = sessionStorage.getItem('theme');
-}
-
-function renderInitialZeroOnDisplay() {
-    CALC_DISPLAY.innerText = 0;
-}
-
-function deleteLastCharacter() {
-    CALC_DISPLAY.innerText = CALC_DISPLAY.innerText.substr(0, CALC_DISPLAY.innerText.length - 1);
-}
-
-function cleanDisplay() {
-    CALC_DISPLAY.innerText = '';
-}
-
-function isCharacterRepeatOneAfterAnoter(character) {
-    let characters = ['.', '+', '-', '*', '/'];
-
-    let result = characters.some(char => char == character);
-    return result;
-}
-
-function renderCharacterOnDisplay(character) {
-    CALC_DISPLAY.innerText += character;
-}
-
-function renderExpressionResultOnDisplay(result) {
-    CALC_DISPLAY.innerText = +result.toFixed(2);
-}
+let specialCharacters = ['.', '+', '-', '*', '/'];
 
 if (sessionStorage.getItem('theme') == null) {
     sessionStorage.setItem('theme', SLIDER.value);
@@ -52,45 +22,151 @@ SLIDER.addEventListener('change', () => {
     switchTheme();
 });
 
-CALC_BODY.addEventListener('click', event => {
-    const KEY_VALUE = event.target.innerText;
+KEYPAD.addEventListener('click', event => {
+    const KEY = event.target;
+    const KEY_TYPE = KEY.dataset.type;
+    const KEY_VALUE = event.target.textContent;
+    const DISPLAY_TEXT = DISPLAY.textContent;
 
-    if (event.target.classList.contains('calc__body')) return;
+    if (event.target.classList.contains('calc__keypad')) return;
 
-    if (KEY_VALUE === 'DEL') {
-        if (CALC_DISPLAY.innerText == 0) return;
+    if (KEY_TYPE === 'delete') {
+        if (DISPLAY_TEXT == '0') return;
 
         deleteLastCharacter();
 
-        if (!CALC_DISPLAY.innerText) renderInitialZeroOnDisplay();
+        if (!DISPLAY.innerText) renderInitialZeroOnDisplay();
         return;
     };
 
-    if (KEY_VALUE === 'RESET') {
+    if (KEY_TYPE === 'reset') {
         cleanDisplay();
         renderInitialZeroOnDisplay();
         return;
     };
 
-    if (KEY_VALUE === '=') {
-        if (CALC_DISPLAY.innerText == 0) return;
+    if (KEY_TYPE === 'equal') {
+        if (DISPLAY_TEXT == '0') return;
 
-        const EXPRESSION = CALC_DISPLAY.innerText;
-        let result = eval(EXPRESSION);
-
-        renderExpressionResultOnDisplay(result);
+        let result = calculateExpression(DISPLAY_TEXT);
+        renderResultOfExpression(result);
         return;
     };
 
-    if (CALC_DISPLAY.innerText.length > 10) return;
-    if (CALC_DISPLAY.innerText == 0) cleanDisplay();
 
-    let renderedCharacters = CALC_DISPLAY.innerText.split('');
-    let lastWellBeCheckedCharacter = renderedCharacters[renderedCharacters.length - 1];
-    let isLastRenderedCharacterRepeat = isCharacterRepeatOneAfterAnoter(lastWellBeCheckedCharacter);
-    let isRecentlyCharacterRepeat = isCharacterRepeatOneAfterAnoter(KEY_VALUE);
 
-    if (isLastRenderedCharacterRepeat && isRecentlyCharacterRepeat) return;
+    if (DISPLAY_TEXT.length > 10) return;
 
-    renderCharacterOnDisplay(KEY_VALUE);
+    if (DISPLAY_TEXT == '0') cleanDisplay();
+
+    if (DISPLAY_TEXT == '0' && KEY_VALUE == '.') {
+        renderCharacter(0);
+        renderCharacter(KEY_VALUE);
+        return;
+    }
+
+    if (specialCharacters.find(char => char === KEY_VALUE)) {
+        if (!DISPLAY_TEXT || DISPLAY_TEXT == '0') {
+            renderInitialZeroOnDisplay();
+            return;
+        }
+
+        let allRenderedCharacters = DISPLAY.innerText.split('');
+        let lastRenderedCharacter = allRenderedCharacters[allRenderedCharacters.length - 1];
+
+        if (Number.isInteger(+lastRenderedCharacter)) {
+            renderCharacter(KEY_VALUE);
+            return;
+        };
+
+        deleteLastCharacter();
+        renderCharacter(KEY_VALUE);
+        return;
+    }
+
+    renderCharacter(KEY_VALUE);
 });
+
+
+
+
+window.addEventListener('keydown', event => {
+    const KEY = event.key;
+    const DISPLAY_TEXT = DISPLAY.textContent;
+
+    if (KEY === '/') event.preventDefault();
+
+    if (KEY === 'Backspace') {
+        if (DISPLAY_TEXT == '0') return;
+
+        deleteLastCharacter();
+
+        if (!DISPLAY.innerText) renderInitialZeroOnDisplay();
+    }
+
+    if (KEY === '=' || KEY === 'Enter') {
+        if (DISPLAY_TEXT == '0') return;
+
+        let result = calculateExpression(DISPLAY_TEXT);
+        renderResultOfExpression(result);
+    };
+
+    if (DISPLAY_TEXT.length > 10) return;
+
+    if (DISPLAY_TEXT == '0' && KEY == '.') {
+        renderCharacter(KEY);
+    }
+
+    if (specialCharacters.find(char => char === KEY)) {
+        let allRenderedCharacters = DISPLAY.innerText.split('');
+        let lastRenderedCharacter = allRenderedCharacters[allRenderedCharacters.length - 1];
+
+        if (Number.isInteger(+lastRenderedCharacter)) {
+            renderCharacter(KEY);
+            return;
+        };
+
+        deleteLastCharacter();
+        renderCharacter(KEY);
+    }
+
+    if (Number.isInteger(+KEY)) {
+        if (DISPLAY_TEXT == '0') cleanDisplay();
+        renderCharacter(KEY);
+    }
+});
+
+
+
+
+
+
+
+function switchTheme() {
+    ROOT_ELEMENT.setAttribute('data-theme', themes[sessionStorage.getItem('theme')]);
+    SLIDER.value = sessionStorage.getItem('theme');
+}
+
+function renderInitialZeroOnDisplay() {
+    DISPLAY.textContent = 0;
+}
+
+function deleteLastCharacter() {
+    DISPLAY.textContent = DISPLAY.textContent.substr(0, DISPLAY.textContent.length - 1);
+}
+
+function cleanDisplay() {
+    DISPLAY.textContent = '';
+}
+
+function renderCharacter(character) {
+    DISPLAY.textContent += character;
+}
+
+function calculateExpression(expression) {
+    return eval(expression);
+}
+
+function renderResultOfExpression(result) {
+    DISPLAY.textContent = +result.toFixed(2);
+}
